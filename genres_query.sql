13ede482917c9f175f3640021780fc83b607d2f2
Shea Faigan <stfaigan@gmail.com>
2022-10-29 15:38:08 -0700

    added queries

---
+++ genres_query.sql
@@ -0,0 +1,15 @@
+WITH movieids_genres AS (
+		SELECT movies.id, is_categorized_as.genreName as genre FROM movies
+			LEFT JOIN is_categorized_as ON movies.id=is_categorized_as.movieId
+	), avg_ratings AS (
+ 		SELECT genre, avg(rates.rating) as avg_rating FROM movieids_genres
+ 			LEFT JOIN rates ON movieids_genres.id=rates.movieId
+ 			GROUP BY genre
+ 	), avg_tag_relevances AS (
+ 		SELECT genre, avg(relates_to.relevance) as avg_tag_relevance FROM movieids_genres
+			INNER JOIN applies ON movieids_genres.id=applies.movieId 
+			LEFT JOIN relates_to ON movieids_genres.id=relates_to.movieId AND applies.tagId=relates_to.tagId
+			GROUP BY genre
+ 	)
+
+SELECT avg_ratings.genre as genre, avg_rating, avg_tag_relevance FROM avg_ratings INNER JOIN avg_tag_relevances ON avg_ratings.genre=avg_tag_relevances.genre
---
+++ movies_query.sql
@@ -0,0 +1,13 @@
+WITH avg_ratings AS (
+ 		SELECT movies.name as movie, avg(rates.rating) as avg_rating FROM movies
+ 			LEFT JOIN rates ON movies.id=rates.movieId
+ 			GROUP BY movie
+ 	), avg_tag_relevances AS (
+ 		SELECT movies.name as movie, avg(relates_to.relevance) as avg_tag_relevance FROM movies
+			INNER JOIN applies ON movies.id=applies.movieId 
+			LEFT JOIN relates_to ON movies.id=relates_to.movieId AND applies.tagId=relates_to.tagId
+			GROUP BY movie
+ 	)
+
+SELECT avg_ratings.movie, avg_rating, avg_tag_relevance FROM avg_ratings
+	INNER JOIN avg_tag_relevances ON avg_ratings.movie=avg_tag_relevances.movie
